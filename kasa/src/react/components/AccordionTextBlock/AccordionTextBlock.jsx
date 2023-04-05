ba1783b6e381e30b5c70f2e7ce03d825467e1280
jbades <jbades@majba.com>
2023-04-05 22:49:51 +0200

    Dealing with toggling matters within Accordion component

--- kasa/src/react/components/AccordionTextBlock/AccordionTextBlock.jsx
+++ kasa/src/react/components/AccordionTextBlock/AccordionTextBlock.jsx
@@ -1,31 +1,66 @@
 import { useState } from "react"
 
-function AccordionTextBlock({blockTitle, blockContent}) {
-    // const [isRolled, setIsRolled] = useState(true)
+function AccordionTextBlock({ blockTitle, blockContent }) {
+    const [isToggled, setIsToggled] = useState(true)
+    const toggledBlockContent = () => {
+        switch (Array.isArray(blockContent)) {
+        case true:
+            return (
+            <div className="housing-element__block">
+                <div className="housing-element__header">
+                    <div>{blockTitle}</div>
+                    <i className="fa-solid fa-chevron-down" onClick={() => setIsToggled(false)}></i>
+                </div>
+                <div className="housing-element__content">
+                    {blockContent.map((object, index) => (
+                        <div key={"object" + index}>{object}</div>
+                    ))}
+                </div>
+            </div>
+            )
+        case false:
+            return (
+            <div className="housing-element__block">
+                <div className="housing-element__header">
+                    <div>{blockTitle}</div>
+                    <i className="fa-solid fa-chevron-down" onClick={() => setIsToggled(false)}></i>
+                </div>
+                <div className="housing-element__content">{blockContent}</div>
+            </div>
+            )
+        default:
+            return null
+        }
+    }
 
-    return typeof blockContent !== "object" ? 
-        <div className="housing-element__block">
-            <div className="housing-element__header">
-                <div>{blockTitle}</div>
-                <i className="fa-solid fa-chevron-down"></i>
+    const untoggledBlockContent = () => {
+        switch (Array.isArray(blockContent)) {
+        case true:
+            return (
+                <div className="housing-element__header">
+                    <div>{blockTitle}</div>
+                    <div className="housing-element__block">
+                    <i className="fa-solid fa-chevron-up" onClick={() => setIsToggled(true)}></i>
+                </div>
             </div>
-            <div className="housing-element__content">{blockContent}</div>
-        </div>
-    :
-        <div className="housing-element__block">
-            <div className="housing-element__header">
-                <div>{blockTitle}</div>
-                <i className="fa-solid fa-chevron-down"></i>
+            )
+        case false:
+            return (
+            <div className="housing-element__block">
+                <div className="housing-element__header">
+                    <div>{blockTitle}</div>
+                    <i className="fa-solid fa-chevron-up" onClick={() => setIsToggled(true)}></i>
+                </div>
             </div>
-            <div className="housing-element__content">
-                {
-                    blockContent && blockContent.map((equipment, index) => {
-                        return <div key={"equipment" + index}>{equipment}</div>
-                    })
-                }
-            </div>
-        </div>
+            )
+        default:
+            return null
+        }
+    }
+
+
+    return isToggled ? toggledBlockContent() : untoggledBlockContent()
 }
 
-export default AccordionTextBlock
+export default AccordionTextBlock;
 
